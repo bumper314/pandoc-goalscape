14d3f6536bd397be7dffb47c6e6b13b75c55b4a3
Steven Audette <me@saudette.net>
2017-01-23 10:48:19 -0700

    MVP dingus.

--- README.md
+++ README.md
@@ -1,3 +1,36 @@
-# pandoc-goalscape
+# pandoc-goalscape-dingus
 
-[Pandoc custom writer](http://johnmacfarlane.net/pandoc/README.html#custom-writers) for generating [Goalscape](http://www.goalscape.com) Project files (.gsp)
+Web dingus for [pandoc-goalscape](/../master/) to convert to [Goalscape](http://www.goalscape.com) Project files (.gsp)
+
+## Example Usage
+
+```markdown
+# Title
+
+## Subgoal 1
+
+This text will appear in the **Notes** tab, which supports:
+
+- *simple* styling
+- [links](http://pandoc.org)
+- and flat lists
+
+## Subgoal 2
+
+## Subgoal 3
+```
+
+The result in Goalscape:
+
+![](/../gh-pages/images/ss-example1.png?raw=true)
+
+
+## Examples To Be Documented
+
+![](/../gh-pages/images/ex-us-constitution.png?raw=true)
+![](/../gh-pages/images/ex-king-james-bible.png?raw=true)
+![](/../gh-pages/images/ex-animals-importance-even.png?raw=true)
+![](/../gh-pages/images/ex-animals-importance-subgoals-even.png?raw=true)
+![](/../gh-pages/images/ex-animals-importance-subgoals.png?raw=true)
+![](/../gh-pages/images/ex-evaluator.png?raw=true)
+![](/../gh-pages/images/ss-naming.png?raw=true)
---
+++ converter.php
@@ -0,0 +1,120 @@
+<?php
+error_reporting(E_ALL);
+
+$pandoc   = realpath("../../.local/bin/") . "/pandoc";
+$writer   = realpath(".") . "/pandoc-goalscape/goalscape.lua";
+$template = realpath(".") . "/pandoc-goalscape/default.goalscape";
+$filter   = realpath(".") . "/pandoc-goalscape/pandoc-filter-goalscape.php";
+
+// Limiting import formats
+$formats = array(
+	'commonmark' => 'CommonMark',
+	'docbook' => 'DocBook',
+	//'docx' => 'Docx (Word)',
+	//'epub' => 'ePub',
+	'haddock' => 'Haddock Markup',
+	'html' => 'HTML',
+	//'json' => 'JSON (Pandoc AST)',
+	'latex' => 'LateX',
+	'markdown' => 'Markdown (Pandoc)',
+	'markdown_strict' => 'Markdown (Strict)',
+	'markdown_github' => 'Markdown (GitHub)',
+	'markdown_mmd' => 'Markdown (MultiMarkdown)',
+	'markdown_phpextra' => 'Markdown (PHP Extra)',
+	'mediawiki' => 'MediaWiki',
+	//'odt' => 'ODT',
+	'opml' => 'OPML',
+	'org' => 'OrgMode (Emacs)',
+	'rst' => 'reStructuredText',
+	'textile' => 'Textile',
+	't2t' => 'Txt2Tags',
+	'twiki' => 'TWiki'
+);
+
+function formatOptions() {
+	$opts = "";
+	global $formats;
+	foreach ($formats as $value => $pretty) {
+		$selected = ($value == 'markdown')?" selected":"";
+		$opts .= <<<EOO
+						<option value="$value"$selected>$pretty</option>
+EOO;
+	}
+	return $opts;
+}
+
+// Convert and download or fallthrough the web form
+$source = '';
+$error = '';
+if(isset($_REQUEST['format']) && isset($_REQUEST['convert_text']) && isset($_REQUEST['source'])) {
+	$format = $_REQUEST['format'];
+	if(!array_key_exists($format, $formats)) {
+		$error = "Unknown source format: $format";
+		goto fallthrough;
+	}
+	$source = mb_convert_encoding($_REQUEST['source'], "UTF-8");
+	// TODO: Limit size
+	if(empty($source)) {
+		$error = "Source text can not be empty";
+		goto fallthrough;
+	}
+	
+	$descriptors = array(
+		0 => array("pipe", "r"),  // stdin
+		1 => array("pipe", "w"),  // stdout
+		2 => array("pipe", "w"),  // stderr
+	);
+	// FIXME: Filter Doesn't run on my server
+	$cmd = "$pandoc --from $format --to $writer --template $template"; // --filter $filter
+	$process = proc_open($cmd, $descriptors, $pipes, "/", null);
+	
+	fwrite($pipes[0], $source);
+	fclose($pipes[0]);
+	
+	$stdout = stream_get_contents($pipes[1]);
+	fclose($pipes[1]);
+	
+	$stderr = stream_get_contents($pipes[2]);
+	fclose($pipes[2]);
+	
+	if(empty($stdout)) {
+		$error = "Failed to convert: $stderr";
+		goto fallthrough;
+	} else { // Download result
+		header('Content-Type: application/download');
+		header('Content-Disposition: attachment; filename="Converted.gsp"');
+		header("Content-Length: " . strlen($stdout));
+		print $stdout;
+		return; // NO FALLTHROUGH
+	}
+}
+fallthrough:
+?>
+<html>
+<head>
+	<title>Convert to Goalscape</title>
+	<link rel="stylesheet" href="style.css" type="text/css" media="screen" />
+</head>
+<body>
+	<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" name="gs" class="form-style-1">
+		<fieldset>
+			<legend>Convert to Goalscape</legend>
+			<ul>
+				<?php echo empty($error)?"":"<li class='error'>" . htmlentities($error) . "</li>"; ?>
+				<li><label>Format:</label>
+					<select name="format" class="field-select">
+						<?php echo formatOptions() ?>
+					</select>
+				</li>
+				<li><label>Source:<textarea name="source" class="field-long field-textarea"><?php echo htmlentities($source) ?></textarea></label></li>
+				<li class="field-buttons">
+					<input type="submit" name="convert_text" value="Download Goalscape Project" />
+				</li>
+			</ul>	
+		</fieldset>
+	</form>
+	<div style="text-align: center">
+		<a href="https://github.com/bumper314/pandoc-goalscape/tree/dingus#example-usage" target="_blank">Example Usage</a>
+	</div>
+</body>
+</html>
